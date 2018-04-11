af88fff574ad346949e8bf15f8fefbf8ba7045f0
gemery <568606291@qq.com>
2018-04-11 23:54:01 +0800

    shen yong rm -rf git add .!  bei ju

---
+++ app/src/main/java/com/example/gemery/groupradioaddfragment/KnowledgeActivity.java
@@ -0,0 +1,209 @@
+package com.example.gemery.groupradioaddfragment;
+
+import android.Manifest;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.example.gemery.groupradioaddfragment.utils.ToastUtil;
+import com.squareup.picasso.Picasso;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.listener.OnBannerListener;
+import com.youth.banner.loader.ImageLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+/**
+ * Created by gemery on 2018/4/10.
+ */
+
+public class KnowledgeActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    @Bind(R.id.mRecyclerView)
+    ListView lv;
+    @Bind(R.id.k_banner)
+    Banner kBanner;
+    @Bind(R.id.taoCi)
+    RadioButton taoCi;
+    @Bind(R.id.zhiNeng)
+    RadioButton zhiNeng;
+    @Bind(R.id.yuShiGui)
+    RadioButton yuShiGui;
+    @Bind(R.id.linYuF)
+    RadioButton linYuF;
+    @Bind(R.id.radiogrop1)
+    RadioGroup radiogrop1;
+    @Bind(R.id.xiuXian)
+    RadioButton xiuXian;
+    @Bind(R.id.wuJin)
+    RadioButton wuJin;
+    @Bind(R.id.shaoShou)
+    RadioButton shaoShou;
+    @Bind(R.id.dMian)
+    RadioButton dMian;
+    @Bind(R.id.radiogroup2)
+    RadioGroup radiogroup2;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_knowledge);
+        ButterKnife.bind(this);
+
+        initBanner(kBanner);
+        changeImageSize();
+    }
+    private void initBanner(Banner banner) {
+        String[] urls = getResources().getStringArray(R.array.url);
+        List list  = Arrays.asList(urls);
+        List<String> titles = Arrays.asList(new String[]{"","","","","qu"});
+        banner.setImageLoader(new ImageLoader() {
+            @Override
+            public void displayImage(Context context, Object path, ImageView imageView) {
+                Picasso.with(KnowledgeActivity.this).load((String)path).into(imageView);
+            }
+        });
+
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE);//设置圆形指示器与标题
+        banner.setIndicatorGravity(BannerConfig.RIGHT);//设置指示器位置
+        banner.setDelayTime(2000);  //设置轮播时间
+        banner.setImages(list);  //设置图片源
+        banner.setBannerTitles(titles);  //设置标题源
+
+
+        banner.setOnBannerListener(new OnBannerListener() {
+            @Override
+            public void OnBannerClick(int position) {
+                ToastUtil.showToast(KnowledgeActivity.this,"您点击了"+ position);;
+            }
+        });
+        banner.start();
+
+        lv.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,
+                new String[]{"这是PDF文档","这是一个word文档","这是Excel文档","这是PPT文档"}));
+
+        lv.setOnItemClickListener(this);
+
+    }
+
+    private void changeImageSize() {
+        //定义底部标签图片大小
+        Drawable drawableFirst = getResources().getDrawable(R.drawable.tc);
+        drawableFirst.setBounds(0, 0, 69, 69);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        taoCi.setCompoundDrawables(null, drawableFirst, null, null);//只放上面
+
+        Drawable drawableSearch = getResources().getDrawable(R.drawable.lyf);
+        drawableSearch.setBounds(0, 0, 69, 69);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        zhiNeng.setCompoundDrawables(null, drawableSearch, null, null);//只放上面
+
+        Drawable drawableMe = getResources().getDrawable(R.drawable.ysg);
+        drawableMe.setBounds(0, 0, 69, 69);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        yuShiGui.setCompoundDrawables(null, drawableMe, null, null);//只放上面
+
+
+        Drawable drawableMe2 = getResources().getDrawable(R.drawable.lyf);
+        drawableMe2.setBounds(0, 0, 69, 69);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        linYuF.setCompoundDrawables(null, drawableMe, null, null);//只放上面
+
+
+        Drawable drawableMe3 = getResources().getDrawable(R.drawable.jq);
+        drawableMe3.setBounds(0, 0, 69, 69);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        wuJin.setCompoundDrawables(null, drawableMe, null, null);//只放上面
+
+
+        Drawable drawableMe4 = getResources().getDrawable(R.drawable.xx);
+        drawableMe4.setBounds(0, 0, 69, 69);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        xiuXian.setCompoundDrawables(null, drawableMe, null, null);//只放上面
+
+
+        Drawable drawableMe5 = getResources().getDrawable(R.drawable.jq);
+        drawableMe5.setBounds(0, 0, 69, 69);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        shaoShou.setCompoundDrawables(null, drawableMe, null, null);//只放上面
+
+
+        Drawable drawableMe6 = getResources().getDrawable(R.drawable.dm);
+        drawableMe6.setBounds(0, 0, 69, 69);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        dMian.setCompoundDrawables(null, drawableMe, null, null);//只放上面
+    }
+
+    @OnClick({R.id.taoCi, R.id.zhiNeng, R.id.yuShiGui, R.id.linYuF, R.id.xiuXian, R.id.wuJin, R.id.shaoShou, R.id.dMian})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.taoCi:
+                lv.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,
+                        new String[]{"这是PDF文档","这是一个word文档","这是Excel文档","这是PPT文档"}));
+                break;
+            case R.id.zhiNeng:
+                lv.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,
+                        new String[]{"1","2","3","4","5","1","2","3","4","5"}));
+                break;
+            case R.id.yuShiGui:
+                break;
+            case R.id.linYuF:
+                break;
+            case R.id.xiuXian:
+                break;
+            case R.id.wuJin:
+                break;
+            case R.id.shaoShou:
+                break;
+            case R.id.dMian:
+                break;
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        String path = null;
+        switch (i){
+            case 0:
+                path = "http://www.hrssgz.gov.cn/bgxz/sydwrybgxz/201101/P020110110748901718161.doc";
+
+                String[] perms = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE};
+//
+//                if (!EasyPermissions.hasPermissions(KnowledgeActivity.this, perms)) {
+//                    EasyPermissions.requestPermissions(KnowledgeActivity.this, "需要访问手机存储权限！", 10086, perms);
+//                } else {
+//                    FileDisplayActivity.show(KnowledgeActivity.this, path);
+//                }
+                break;
+            case 1:
+                path =  "/storage/emulated/0/test.docx";
+                break;
+            case 2:
+                path = "/storage/emulated/0/test.txt";
+                break;
+
+            case 3:
+                path = "/storage/emulated/0/test.xlsx";
+                break;
+            case 4:
+                path = "/storage/emulated/0/test.pptx";
+                break;
+
+            case 5:
+                path = "/storage/emulated/0/test.pdf";
+                break;
+                default:
+        }
+
+
+    }
+}
--- app/src/main/java/com/example/gemery/groupradioaddfragment/fragment/HomeFragment.java
+++ app/src/main/java/com/example/gemery/groupradioaddfragment/fragment/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.gemery.groupradioaddfragment.fragment;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,6 +12,7 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.SimpleAdapter;
 
+import com.example.gemery.groupradioaddfragment.KnowledgeActivity;
 import com.example.gemery.groupradioaddfragment.R;
 
 import java.util.ArrayList;
@@ -54,11 +56,11 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemClickLis
 
         //图标
         int icno[] = {R.drawable.th_text3, R.drawable.th_topple3, R.drawable.th_toy_bot,
-                R.drawable.th_wikipanion2, R.drawable.th_yelp, R.drawable.th_youtube, R.drawable.th_textreader,
-                R.drawable.th_thisday, R.drawable.th_tnjspsng4hhhj3bd32ae, R.drawable.th_wedict};
+                R.drawable.th_wikipanion2, R.drawable.th_yelp, R.drawable.th_youtube,
+                R.drawable.th_textreader, R.drawable.th_thisday};
         //图标下的文字
-        String name[] = {"新闻公告", "名店系统", "客服系统", "流程审批", "考勤签到",
-                "报表分析", "记出差申请", "请假申请", "工作日志", "我的提成"};
+        String name[] = {"配送中心", "安装中心", "结算中心", "功勋榜", "知识中心",
+                "库存查询", "通讯录", "个人中心"};
         dataList = new ArrayList<Map<String, Object>>();
         for (int i = 0; i < icno.length; i++) {
             Map<String, Object> map = new HashMap<String, Object>();
@@ -71,7 +73,14 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemClickLis
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        builder.setTitle("提示").setMessage(dataList.get(i).get("text").toString()).create().show();
+        if(i == 4){
+            Intent intent = new Intent(getActivity(),KnowledgeActivity.class);
+            intent.putExtra("action","to_knowledge_activity");
+            getActivity().startActivity(intent);
+        }else {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            builder.setTitle("提示").setMessage(dataList.get(i).get("text").toString()).create().show();
+        }
     }
 }
